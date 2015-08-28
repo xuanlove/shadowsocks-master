472be63fac887b2064a7aee9581cd29b4725103b
xuanlove <chenyuxuanlove@gmail.com>
2015-08-29 03:37:40 +0800

    1.0

---
+++ shadowsocks/server.py
@@ -0,0 +1,16 @@
+import time
+import sys
+import thread
+import server_pool
+import db_transfer
+
+#def test():
+#    thread.start_new_thread(DbTransfer.thread_db, ())
+#    Api.web_server()
+
+if __name__ == '__main__':
+    #server_pool.ServerPool.get_instance()
+    #server_pool.ServerPool.get_instance().new_server(2333, '2333')
+    thread.start_new_thread(db_transfer.DbTransfer.thread_db, ())
+    while True:
+        time.sleep(99999)
